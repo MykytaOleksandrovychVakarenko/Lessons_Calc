bf8d07c3ca5d5fd51cea8dd445eb3122ef64db05
Nikita  Vakarenko <138662704+usernamedudh@users.noreply.github.com>
2025-09-19 17:18:23 +0300

    Add project files.

---
+++ Lessons_Switch/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessons_Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Operation (+, -, *, /):");
+            string operation = Console.ReadLine();
+
+            switch(operation)
+            {
+                case "+":
+                    Console.WriteLine($"Result: {number1 + number2}");
+                    break;
+                case "-":
+                    Console.WriteLine($"Result: {number1 - number2}");
+                    break;
+                case "*":
+                    Console.WriteLine($"Result: {number1 * number2}");
+                    break;
+                case "/":
+                    if (number2 != 0)
+                    {
+                        Console.WriteLine($"Result: {number1 / number2}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is not allowed.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Error: Invalid operation.");
+                    break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lessons_Switch/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Lessons_Switch")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lessons_Switch")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("866dfc7c-a78b-4804-acab-f83cfd99df73")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
